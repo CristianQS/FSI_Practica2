ec7b7115a3f1e67eb96d8f54f1a0bd63d07b063d
cristian <cristian_31teror@hotmail.com>
2017-05-01 20:26:50 +0100

    Revisar Mnist

---
+++ nn_iris.py
@@ -0,0 +1,95 @@
+import tensorflow as tf
+import numpy as np
+
+
+# Translate a list of labels into an array of 0's and one 1.
+# i.e.: 4 -> [0,0,0,0,1,0,0,0,0,0]
+def one_hot(x, n):
+    """
+    :param x: label (int)
+    :param n: number of bits
+    :return: one hot code
+    """
+    if type(x) == list:
+        x = np.array(x)
+    x = x.flatten()
+    o_h = np.zeros((len(x), n))
+    o_h[np.arange(len(x)), x] = 1
+    return o_h
+
+
+data = np.genfromtxt('iris.data', delimiter=",")  # iris.data file loading
+np.random.shuffle(data)  # we shuffle the data
+x_data = data[:, 0:4].astype('f4')  # the samples are the four first rows of data
+y_data = one_hot(data[:, 4].astype(int), 3)  # the labels are in the last row. Then we encode them in one hot code
+
+x_training = x_data[:int(0.7 * len(x_data)), ]
+y_training = y_data[:int(0.7 * len(y_data)), ]
+
+x_validation = x_data[int(0.7 * len(x_data)):int(0.85 * len(x_data)), ]
+y_validation = y_data[int(0.7 * len(y_data)):int(0.85 * len(y_data)), ]
+
+x_test = x_data[int(0.85 * len(x_data)):, ]
+y_test = y_data[int(0.85 * len(y_data)):, ]
+
+print "\nSome samples..."
+print len(x_data)
+print len(y_data)
+for i in range(20):
+    print x_data[i], " -> ", y_data[i]
+print
+
+x = tf.placeholder("float", [None, 4])  # samples
+y_ = tf.placeholder("float", [None, 3])  # labels
+
+W1 = tf.Variable(np.float32(np.random.rand(4, 5)) * 0.1)
+b1 = tf.Variable(np.float32(np.random.rand(5)) * 0.1)
+
+W2 = tf.Variable(np.float32(np.random.rand(5, 3)) * 0.1)
+b2 = tf.Variable(np.float32(np.random.rand(3)) * 0.1)
+
+h = tf.nn.sigmoid(tf.matmul(x, W1) + b1)
+# h = tf.matmul(x, W1) + b1  # Try this!
+y = tf.nn.softmax(tf.matmul(h, W2) + b2)
+
+loss = tf.reduce_sum(tf.square(y_ - y))
+
+train = tf.train.GradientDescentOptimizer(0.01).minimize(loss)  # learning rate: 0.01
+
+init = tf.initialize_all_variables()
+
+sess = tf.Session()
+sess.run(init)
+
+print "----------------------"
+print "   Start training...  "
+print "----------------------"
+
+batch_size = 20
+
+for epoch in xrange(200):
+    for jj in xrange(len(x_training) / batch_size):
+        batch_xs = x_training[jj * batch_size: jj * batch_size + batch_size]
+        batch_ys = y_training[jj * batch_size: jj * batch_size + batch_size]
+        sess.run(train, feed_dict={x: batch_xs, y_: batch_ys})
+
+    print "Epoch #:", epoch, "Error: ", sess.run(loss, feed_dict={x: x_validation, y_: y_validation})
+    result = sess.run(y, feed_dict={x: batch_xs})
+    for b, r in zip(batch_ys, result):
+        print b, "-->", r
+
+    print "----------------------------------------------------------------------------------"
+
+print "----------------------"
+print "        TEST  "
+print "----------------------"
+
+test = sess.run(loss, feed_dict={x: x_test, y_: y_test})
+print "Error del test ha sido de: ", test
+
+result = sess.run(y, feed_dict={x: x_test})
+for b, r in zip(y_test, result):
+    print b, "-->", r
+
+
+
---
+++ nn_mnist.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+import gzip
+import pickle
+
+import tensorflow as tf
+import numpy as np
+
+# Translate a list of labels into an array of 0's and one 1.
+# i.e.: 4 -> [0,0,0,0,1,0,0,0,0,0]
+def one_hot(x, n):
+    """
+    :param x: label (int)
+    :param n: number of bits
+    :return: one hot code
+    """
+    if type(x) == list:
+        x = np.array(x)
+    x = x.flatten()
+    o_h = np.zeros((len(x), n))
+    o_h[np.arange(len(x)), x] = 1
+    return o_h
+
+
+f = gzip.open('mnist.pkl.gz', 'rb')
+train_set, valid_set, test_set = pickle.load(f)#,encoding='latin1')
+f.close()
+
+# TODO: the neural net!!
+
+x_training, y_training = train_set
+x_validation, y_validation = valid_set
+x_test, y_test = test_set
+
+y_training = one_hot(y_training,10)
+y_validation = one_hot(y_validation,10)
+y_test = one_hot(y_test,10)
+
+
+x = tf.placeholder(tf.float32, [None, 784])  # samples
+y_ = tf.placeholder(tf.float32, [None, 10])  # labels
+
+W1 = tf.Variable(np.float32(np.random.rand(784, 100)) * 0.1)
+b1 = tf.Variable(np.float32(np.random.rand(100)) * 0.1)
+
+W2 = tf.Variable(np.float32(np.random.rand(100, 10)) * 0.1)
+b2 = tf.Variable(np.float32(np.random.rand(10)) * 0.1)
+
+
+h = tf.nn.sigmoid(tf.matmul(x, W1) + b1)
+# h = tf.matmul(x, W1) + b1  # Try this!
+y = tf.nn.softmax(tf.matmul(h, W2) + b2)
+
+loss = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+train = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
+
+init = tf.initialize_all_variables()
+
+sess = tf.Session()
+sess.run(init)
+
+print "----------------------"
+print "   Start training...  "
+print "----------------------"
+
+batch_size = 20
+# Train
+
+for epoch in xrange(10):
+    for jj in xrange(len(x_training) / batch_size):
+        batch_xs = x_training[jj * batch_size: jj * batch_size + batch_size]
+        batch_ys = y_training[jj * batch_size: jj * batch_size + batch_size]
+        sess.run(train, feed_dict={x: batch_xs, y_: batch_ys})
+
+    print "Epoch #:", epoch, "Error: ", sess.run(loss, feed_dict={x: x_validation, y_: y_validation})
+    result = sess.run(y, feed_dict={x: batch_xs})
+    for b, r in zip(batch_ys, result):
+        print b, "-->", r
+
+    print "----------------------------------------------------------------------------------"
+
+print "----------------------"
+print "        TEST  "
+print "----------------------"
+
+test = sess.run(loss, feed_dict={x: x_test, y_: y_test})
+print "Error del test ha sido de: ", test
+
